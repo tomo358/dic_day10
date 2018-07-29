ecba31e5c33ae201cc014609ab12ac6b33a21568
KazuhiroObama <obamakazuhiro@gmail.com>
2018-07-29 11:52:52 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/users/sign_in', to: 'sessions#new'
+  resources :sessions, only: [:create, :destroy]
   resources :users
   root "blogs#index"
   resources :blogs
-
 end
