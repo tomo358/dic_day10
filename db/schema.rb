4cf279edf5ce962fbfc0d08128412aa2b0d0db98
KazuhiroObama <obamakazuhiro@gmail.com>
2018-07-29 11:28:23 +0900

    Userでsccafold。パスワードカラム追加。gem 'bcrypt'追記。Userモデルに'has_secure_password'追加。show,indeでパスワードの表示削除。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180729015045) do
+ActiveRecord::Schema.define(version: 20180729021725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20180729015045) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
